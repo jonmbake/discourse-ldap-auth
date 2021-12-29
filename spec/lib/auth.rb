073cb00d996ca5cf9202af9c048800fbf8c0ef6e
David Taylor <david@taylorhq.com>
2021-12-29 16:17:07 +0000

    Update omit_username -> override_username following core changes
    This commit maintains backwards compatibility with older Discourse changes

--- spec/lib/auth.rb
+++ spec/lib/auth.rb
@@ -17,7 +17,7 @@ module Auth
                   :email_valid, :extra_data, :awaiting_activation,
                   :awaiting_approval, :authenticated, :authenticator_name,
                   :requires_invite, :not_allowed_from_ip_address,
-                  :admin_not_allowed_from_ip_address, :omit_username
+                  :admin_not_allowed_from_ip_address, :overrides_username
 
     attr_accessor :failed,
                   :failed_reason
