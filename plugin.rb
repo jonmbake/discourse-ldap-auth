109aba5237ac821ebd78183714441500cc37710b
Jon Bake <jonmbake@gmail.com>
2025-02-28 09:52:01 -0600

    Bump net-ldap version

--- plugin.rb
+++ plugin.rb
@@ -7,7 +7,7 @@ enabled_site_setting :ldap_enabled
 
 gem 'pyu-ruby-sasl', '0.0.3.3', require: false
 gem 'rubyntlm', '0.3.4', require: false
-gem 'net-ldap', '0.17.1'
+gem 'net-ldap', '0.18.0'
 
 require 'yaml'
 require_relative 'lib/omniauth-ldap/adaptor'
